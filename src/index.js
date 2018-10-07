31929e561d6b3b3d449e96c8d878ccb229d8ab02
pirs <pirsfrommi6@gmail.com>
2018-10-07 21:17:46 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    if (array.length == 0) return 0;
+
+    const searcharray = new Set(array);
+    let max = 1;
+    let startlm = 0;
+    const setelem = new Set();
+
+    for (let key of searcharray) {
+        if (!searcharray.has(key-1) && searcharray.has(key+1)) setelem.add(key);
+    }
+
+    if (setelem.length == 0) return 1;
+    
+    for (let key of setelem) {
+    	startlm = key;                 
+        let endlm   = startlm;          
+        while(searcharray.has(endlm)){
+            endlm++;                  
+        }
+        if(max > endlm-startlm){
+        	 max = max;
+        }else{
+        	 max = endlm-startlm;
+        }
+      } 
+   return max;
 }
